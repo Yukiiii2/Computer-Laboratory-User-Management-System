7be3d3d9fcad5f70e3de5108f72cb16cd4531ae8
Yuki <142190110+Yukiiii2@users.noreply.github.com>
2023-12-18 21:51:28 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { onBeforeMount } from 'vue';
+import { useRouter } from 'vue-router';
+const router = useRouter();
+
+onBeforeMount(() => 
+    {router.push('/Login')}
+);
+</script>
+
+<template>
+    <div>
+        <router-view></router-view>  
+    </div>
+</template>
+
+<style>
+
+</style>
