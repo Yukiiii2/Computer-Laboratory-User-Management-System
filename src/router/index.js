433e5c20908dbfd9eba6172415514e398abf6c8a
Yuki <142190110+Yukiiii2@users.noreply.github.com>
2023-12-19 14:38:33 +0800

    Add files via upload

---
+++ src/router/index.js
@@ -0,0 +1,42 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import Login from "../components/Login.vue";
+import register from "../components/register.vue";
+import Page1 from "../components/Page1.vue";
+import Page2 from "../components/Page2.vue";
+import navbar from "../components/navbar.vue";
+
+const routes = [
+    {
+      path: "/Login",
+      name: "Login",
+      component: Login,
+    },
+    {
+      path: "/register",
+      name: "register",
+      component: register,
+    },
+    {
+      path: "/Page1",
+      name: "Page1",
+      component: Page1,
+    },
+    {
+      path: "/Page2",
+      name: "Page2",
+      component: Page2,
+    },
+    {
+      path: "/navbar",
+      name: "NavBar",
+      component: navbar,
+    },
+  ];
+
+  const router = createRouter({
+    history: createWebHistory(),
+    routes,
+  });
+
+  export default router;
