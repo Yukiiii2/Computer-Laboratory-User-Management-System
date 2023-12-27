1389e52c6334b654d0ce9c6f0fe2726118b748cb
Yuki <142190110+Yukiiii2@users.noreply.github.com>
2023-12-28 02:21:57 +0800

    Add files via upload

---
+++ src/components2/Reports.vue
@@ -0,0 +1,194 @@
+<template>
+    <navbar/>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+        <div class="main">
+          <div class="wrapper">
+            <p class="name">MARC COSTILLAS</p>
+            <p class="section">BSIT - 2A</p>
+          </div>
+    
+          <div class="outer">
+            <div class="records">REPORTS</div>
+    
+            <div class="bgm">
+                    <div class="information">
+                        <div class="unit">
+                            <h2>PC #14/ L202</h2>
+                        </div>
+                        <div class="date">
+                            <h2>12/31/23</h2>
+                        </div>
+                    </div>
+                        
+                    <div class="problem">
+                            <p>The ethernet cable of this Computer Unit is not connected</p>
+                    </div>
+
+                    <div class="check">
+                            <button><i class='bx bx-check-circle'></i></button> <button><i class='bx bx-x-circle' ></i></button>
+                    </div>
+            </div>
+
+            <div class="bgm">
+                    <div class="information">
+                        <div class="unit">
+                            <h2>PC #09/ L203</h2>
+                        </div>
+                        <div class="date">
+                            <h2>08/06/23</h2>
+                        </div>
+                    </div>
+                        
+                    <div class="problem">
+                            <p>The Monitor is missing</p>
+                    </div>
+
+                    <div class="check">
+                            <button><i class='bx bx-check-circle'></i></button> <button><i class='bx bx-x-circle' ></i></button>
+                    </div>
+            </div>
+
+                        <div class="bgm">
+                    <div class="information">
+                        <div class="unit">
+                            <h2>PC #123/ L201</h2>
+                        </div>
+                        <div class="date">
+                            <h2>09/19/23</h2>
+                        </div>
+                    </div>
+                        
+                    <div class="problem">
+                            <p>The Monitor has a Bluescreen Display</p>
+                    </div>
+
+                    <div class="check">
+                            <button><i class='bx bx-check-circle'></i></button> <button><i class='bx bx-x-circle' ></i></button>
+                    </div>
+            </div>
+          </div>
+        </div>
+    
+    
+    
+    </template>
+    
+    
+    <script>
+      import navbar from "../components2/navbar.vue";
+      import { useRouter } from 'vue-router';
+      
+      export default {
+        components: {
+          navbar,
+        },
+        setup() {
+          const router = useRouter();
+      
+          const newScreen = (nscr) => {
+            router.push(nscr);
+          };
+      
+          return {
+            newScreen,
+          };
+        },
+      };
+    </script>
+    
+    
+    
+    <style>
+    html {
+        height: 100%;
+        width: 100%
+    }
+    
+    *{
+        font-family: Verdana, Geneva, Tahoma, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+    }
+    
+    .wrapper{
+        box-shadow: 5px 5px 5px 6px #000000;
+        background-color:rgb(254, 254, 254);
+        height: 200px;
+        width: 1450px;
+        position: fixed;
+        top: 50px;
+        margin-left: 400px;
+        border-radius: 15px;
+    }
+    
+    .main .wrapper .name{
+        position: relative;
+        left: -480px;
+        top: -25px;
+        font-weight: bolder;
+        font-size: 45px;
+    }
+    
+    .main .wrapper .section{
+        position: relative;
+        left: -612px;
+        top: -10px;
+        font-weight: 400;
+        font-size: 35px;
+    }
+    
+    .outer{
+      position: relative;
+      top: 300px;
+      left: 100px;
+      margin-left: 350px;
+      width: 1330px;
+      height: 600px;
+      border: 2px solid #FB0664;
+      box-shadow: 5px 5px 3px #000000;
+    }
+    
+    .records{
+      color: white;
+      background-color: #FB0664;
+      display: flex;
+      justify-content: flex-start; 
+      font-size: 55px;
+    }
+
+    .information{
+        position: relative;
+        left: -480px;
+    }
+
+    h2{
+        font-weight: bold;
+        color: #FB0664;
+    }
+
+    .problem{
+        position: relative;
+        top: -60px;
+    }
+
+    p{
+        font-weight: bold;
+
+    }
+
+    .check{
+        position: relative;
+        top: -120px;
+        left: -180px;
+    }
+
+    .bgm{
+        margin-top: 15px;
+        position: relative;
+        top: 10px;
+        height: 120px;
+        border: 2px solid #FB0664;
+        border-style: solid none;
+    }
+    </style>
---
+++ src/components2/navbar.vue
@@ -0,0 +1,115 @@
+<template>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+        <div class="sidenav">
+
+            <img src="../assets/navlogo.png" alt="" class="navlogo1">
+
+            <div class="box">
+                <Button @click="newScreen('/attendance')" >Attendance<i class='bx bx-group'></i></Button>
+            </div>
+    
+            <div class="box">
+                <Button @click="newScreen('/account')" >Account<i class='bx bx-user' ></i></Button>
+            </div>
+
+            <div class="box">
+                <Button @click="newScreen('/records')" >Records<i class='bx bx-receipt'></i></Button>
+            </div>
+
+            <div class="box">
+                <Button @click="newScreen('/reports')" >Reports<i class='bx bxs-report' ></i></Button>
+            </div>
+    
+            <div class="box">
+                <Button @click="newScreen('/Login')" class="logout">Logout<i class='bx bx-log-out-circle' ></i></Button>
+            </div>
+        </div>
+    </template>
+    
+
+    <script setup>
+    import { useRouter } from 'vue-router';
+    const router = useRouter();
+    const newScreen = (nscr) => {
+    router.push(nscr)
+    };
+    
+    </script>
+    
+    
+    
+    <style>
+    html {
+        height: 100%;
+        width: 100%
+    }
+    
+    button{
+        background: none;
+        border: none;
+        padding: 0;
+    }
+    i{
+        margin-left: 30px;
+    }
+
+    *{
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    }
+    
+    .sidenav {
+      height: 100%;
+      width: 330px;
+      position: fixed;
+      z-index: 1;
+      top: 0;
+      left: 0;
+      background-color:#FB0664;
+      overflow-x: hidden;
+      padding-top: 170px;
+    }
+    
+    .sidenav button {
+      padding: 25px 12px 25px 32px;
+      text-decoration: none;
+      font-size: 30px;
+      color: white;
+      display: block;
+      font-weight: bolder;
+      margin-top: 20px;
+    }
+    
+    button:hover{
+        color: #FB0664E0;
+    }
+    
+    .box{
+        width: 350px;
+        margin-left: 30px;
+        border-bottom: 2px solid white;
+        -webkit-transition: all 0.5s ease;
+        -moz-transition: all 0.5s ease;
+        -ms-transition: all 0.5s ease;
+        transition: all 0.5s ease;
+    }
+    
+    .box:hover{
+        border-radius: 60px;
+        background-color: white;
+    }
+    
+    .navlogo1{
+        position: relative;
+        top: -100px;
+        width: 150px;
+        filter: brightness(0) invert(1);
+    }
+
+    @media screen and (max-height: 450px) {
+      .sidenav {padding-top: 15px;}
+      .sidenav a {font-size: 18px;}
+    }
+    </style>
