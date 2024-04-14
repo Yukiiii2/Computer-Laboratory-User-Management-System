68eb7883597f6487090cdafca6429cb320bee87f
Yuki <142190110+Yukiiii2@users.noreply.github.com>
2024-04-14 23:51:45 +0800

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
 <script setup>
 import { onBeforeMount } from 'vue';
 import { useRouter } from 'vue-router';
+import axios from 'axios'
 const router = useRouter();
 
 onBeforeMount(() => 
     {router.push('/Login')}
 );
+
+const fetch = async () => {
+    
+    try { 
+        const response = await axios.get('http://127.0.0.1:8000/api/student');
+        console.log(response.data) //Return  val
+    } catch (error) {
+        console.error('Error Message:', error);
+    } finally {        
+    }
+};
+
+fetch()
 </script>
 
 <template>
@@ -17,3 +31,4 @@ onBeforeMount(() =>
 <style>
 
 </style>
+
